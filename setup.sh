ca1f0c606e74f978aa239a4c5ef3fedc60c9dd2c
Sean Choi <yo2seol@gmail.com>
2018-12-08 22:03:25 +0000

    Adding new configuration for hosts.ini

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ansible-playbook -i inventory/mycluster/hosts.ini --become --become-user=root cluster.yml
+ansible-playbook -i inventory/mycluster/hosts.ini --flush-cache --become --become-user=root cluster.yml
